a909fa3854ad4e4c454ec0b367be02d375cd1cad
Madarga <madarga@email.com>
2019-08-13 13:36:57 +0800

    Class-based views

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,14 @@ from .models import Post
 
 class PostList(TemplateView):
     template_name = 'blog/post_list.html'
+
     def get(self, request, *args, **kwargs):
         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
         return render(request, self.template_name, {'posts':posts})
 
 class PostDetail(TemplateView):
     template_name = 'blog/post_detail.html'
+    
     def get(self, request, *args, **kwargs):
         post = get_object_or_404(Post, pk=kwargs.get("pk"))
         return render(request, self.template_name, {'post':post})
@@ -21,11 +23,6 @@ class PostDetail(TemplateView):
 class PostEdit(TemplateView):
     template_name = 'blog/post_edit.html'
     
-    def get(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, pk=kwargs.get("pk"))
-        form = PostForm(instance=post)
-        return render(request, self.template_name, {'form':form})
-
     def post(self, request, *args, **kwargs):
         post = get_object_or_404(Post, pk=kwargs.get("pk"))
         form = PostForm(request.POST, instance=post)
@@ -35,10 +32,17 @@ class PostEdit(TemplateView):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+    
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs.get("pk"))
+        form = PostForm(instance=post)
+        return render(request, self.template_name, {'form':form})
+
             
 
 class PostNew(TemplateView):
     template_name = 'blog/post_edit.html'
+
     def post(self, request, *args, **kwargs):
         form = PostForm(request.POST)
         if form.is_valid():
